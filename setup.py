74269b882789bbb4b292ffb180c934385e34eca3
Md Manawar Iqbal <manawariqbal@Mohmmeds-MacBook-Pro.local>
2025-11-30 00:07:30 +0530

    Project structure readt venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Md Manawar Iqbal",
+    author_email="mmiqbalmanawar@gmail.com",
+    packages=find_packages()
+)
